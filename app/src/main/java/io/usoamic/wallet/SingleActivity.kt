c5b5312f4610862f011afe2366014603135efb8a
primys <a2uso@tuta.io>
2020-04-23 13:18:30 -0700

    Add navigation by androidx navigation component

--- app/src/main/java/io/usoamic/wallet/SingleActivity.kt
+++ app/src/main/java/io/usoamic/wallet/SingleActivity.kt
@@ -9,10 +9,5 @@ class SingleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, AuthFragment.newInstance())
-                    .commitNow()
-        }
     }
 }
