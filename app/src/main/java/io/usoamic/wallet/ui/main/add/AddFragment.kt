be004df34b62e347a20a62fba682c758d16e603d
primys <a2uso@tuta.io>
2020-04-23 12:22:10 -0700

    Add AddFragment

---
+++ app/src/main/java/io/usoamic/wallet/ui/main/add/AddFragment.kt
@@ -0,0 +1,11 @@
+package io.usoamic.wallet.ui.main.add
+
+import androidx.fragment.app.viewModels
+import io.usoamic.wallet.R
+import io.usoamic.wallet.ui.base.BaseFragment
+
+class AddFragment : BaseFragment(R.layout.fragment_add) {
+    override val viewModel: AddViewModel by viewModels()
+
+
+}
---
+++ app/src/main/java/io/usoamic/wallet/ui/main/add/AddViewModel.kt
@@ -0,0 +1,6 @@
+package io.usoamic.wallet.ui.main.add
+
+import io.usoamic.wallet.ui.base.BaseViewModel
+
+class AddViewModel : BaseViewModel() {
+}
