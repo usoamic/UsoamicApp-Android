0ed1eacf7502b61dc685c68b710548cc09607acf
primys <a2uso@tuta.io>
2020-04-23 12:47:06 -0700

    Add hideKeyboard() method, binding and navigator variable to BaseFragment, and other

--- app/src/main/java/io/usoamic/wallet/ui/base/BaseFragment.kt
+++ app/src/main/java/io/usoamic/wallet/ui/base/BaseFragment.kt
@@ -1,12 +1,16 @@
 package io.usoamic.wallet.ui.base
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import io.usoamic.wallet.R
 import io.usoamic.wallet.extensions.observe
 import io.usoamic.wallet.extensions.string
@@ -16,7 +20,13 @@ abstract class BaseFragment(
     @LayoutRes val layoutRes: Int
 ) : Fragment() {
     protected abstract val viewModel: BaseViewModel
+    protected abstract val binding: Any
+
     protected lateinit var errorDialog: AlertDialog
+
+    protected val navigator: NavController by lazy {
+        NavHostFragment.findNavController(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,13 +37,18 @@ abstract class BaseFragment(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setObservers()
+        initObservers()
+        initListeners()
     }
 
-    protected open fun setObservers() {
+    protected open fun initObservers() {
         observe(viewModel.ldThrowable, ::showErrorDialog)
         observe(viewModel.ldError, ::showErrorDialog)
         observe(viewModel.ldProgress, ::showProgress)
+    }
+
+    protected open fun initListeners() {
+
     }
 
     protected open fun showProgress(isProgress: Boolean) = Unit
@@ -48,6 +63,15 @@ abstract class BaseFragment(
     }
 
     protected open fun showErrorDialog(error: Throwable) = showErrorDialog(error.message ?: string(R.string.unknown_error))
+
+    protected fun hideKeyboard() {
+        //Source: https://stackoverflow.com/questions/1109022/close-hide-android-soft-keyboard
+        activity?.let {
+            val imm = it.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+        }
+
+    }
 
     override fun onDestroyView() {
         if(::errorDialog.isInitialized) {
