a3832a2f11275b63aa8dc0f751dd8892dcec1018
primys <a2uso@tuta.io>
2020-04-23 12:18:56 -0700

    Add features to base classes for Fragment and ViewModel

--- app/src/main/java/io/usoamic/wallet/ui/base/BaseFragment.kt
+++ app/src/main/java/io/usoamic/wallet/ui/base/BaseFragment.kt
@@ -1,19 +1,58 @@
 package io.usoamic.wallet.ui.base
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import io.usoamic.wallet.R
+import io.usoamic.wallet.extensions.observe
+import io.usoamic.wallet.extensions.string
 
-open class BaseFragment(
+
+abstract class BaseFragment(
     @LayoutRes val layoutRes: Int
 ) : Fragment() {
+    protected abstract val viewModel: BaseViewModel
+    protected lateinit var errorDialog: AlertDialog
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(layoutRes, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setObservers()
+    }
+
+    protected open fun setObservers() {
+        observe(viewModel.ldThrowable, ::showErrorDialog)
+        observe(viewModel.ldError, ::showErrorDialog)
+        observe(viewModel.ldProgress, ::showProgress)
+    }
+
+    protected open fun showProgress(isProgress: Boolean) = Unit
+
+    protected open fun showErrorDialog(error: String) {
+        errorDialog = AlertDialog.Builder(requireContext())
+            .setTitle(string(R.string.error))
+            .setMessage(error)
+            .setPositiveButton(android.R.string.ok, null)
+            .setIcon(R.drawable.ic_launcher_foreground)
+            .show()
+    }
+
+    protected open fun showErrorDialog(error: Throwable) = showErrorDialog(error.message ?: string(R.string.unknown_error))
+
+    override fun onDestroyView() {
+        if(::errorDialog.isInitialized) {
+            errorDialog.dismiss()
+        }
+        super.onDestroyView()
     }
 }
--- app/src/main/java/io/usoamic/wallet/ui/base/BaseViewModel.kt
+++ app/src/main/java/io/usoamic/wallet/ui/base/BaseViewModel.kt
@@ -1,7 +1,11 @@
 package io.usoamic.wallet.ui.base
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 open class BaseViewModel : ViewModel() {
+    val ldProgress = MutableLiveData<Boolean>()
+    val ldThrowable = MutableLiveData<Throwable>()
+    val ldError = MutableLiveData<String>()
 
 }
