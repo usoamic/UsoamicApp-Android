5054097721acec4c17803193b233072c41538edf
primys <a2uso@tuta.io>
2020-04-24 13:30:05 -0700

    Added di AppComponent

---
+++ app/src/main/java/io/usoamic/wallet/di/AppComponent.kt
@@ -0,0 +1,20 @@
+package io.usoamic.wallet.di
+
+import dagger.Component
+import io.usoamic.wallet.SingleActivity
+import io.usoamic.wallet.di.modules.RepositoryModule
+import io.usoamic.wallet.di.modules.UsoamicModule
+import io.usoamic.wallet.di.scopes.AppScope
+import io.usoamic.wallet.di.subcomponents.add.AddSubcomponent
+import io.usoamic.wallet.di.subcomponents.auth.AuthSubcomponent
+import javax.inject.Singleton
+
+@Singleton
+@AppScope
+@Component(modules = [UsoamicModule::class, RepositoryModule::class])
+interface AppComponent {
+    val addSubcomponent: AddSubcomponent.Factory
+    val authSubcomponent: AuthSubcomponent
+
+    fun inject(activity: SingleActivity)
+}
