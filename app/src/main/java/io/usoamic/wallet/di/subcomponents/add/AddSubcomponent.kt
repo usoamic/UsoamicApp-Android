385727192a31871c757929008288b754c10cc936
primys <a2uso@tuta.io>
2020-04-24 13:29:53 -0700

    Add subcomponents for Auth and Add fragments

---
+++ app/src/main/java/io/usoamic/wallet/di/subcomponents/add/AddSubcomponent.kt
@@ -0,0 +1,18 @@
+package io.usoamic.wallet.di.subcomponents.add
+
+import dagger.BindsInstance
+import dagger.Subcomponent
+import io.usoamic.wallet.di.scopes.AuthScope
+import io.usoamic.wallet.domain.models.AppArguments
+import io.usoamic.wallet.ui.main.add.AddFragment
+
+@AuthScope
+@Subcomponent
+interface AddSubcomponent {
+    fun inject(addFragment: AddFragment)
+
+    @Subcomponent.Factory
+    interface Factory {
+        fun create(@BindsInstance args: AppArguments.Add): AddSubcomponent
+    }
+}
