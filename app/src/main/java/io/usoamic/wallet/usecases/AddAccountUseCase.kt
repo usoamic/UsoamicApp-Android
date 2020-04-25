1659b297bd259fc6b2f776e31587f0f5ad808271
primys <a2uso@tuta.io>
2020-04-25 11:56:39 -0700

    Added RealmRepository argument

--- app/src/main/java/io/usoamic/wallet/usecases/AddAccountUseCase.kt
+++ app/src/main/java/io/usoamic/wallet/usecases/AddAccountUseCase.kt
@@ -1,12 +1,16 @@
 package io.usoamic.wallet.usecases
 
 import io.reactivex.rxjava3.core.Single
+import io.usoamic.wallet.domain.repositories.RealmRepository
 import io.usoamic.wallet.domain.repositories.TokenRepository
 import java.math.BigDecimal
 import javax.inject.Inject
 
 class AddAccountUseCase @Inject constructor(
-    private val tokenRepository: TokenRepository
+    private val mTokenRepository: TokenRepository,
+    private val mRealmRepository: RealmRepository
 ) {
-    fun getSupply(): Single<BigDecimal> = tokenRepository.getSupply()
+    fun getSupply(): Single<BigDecimal> = mTokenRepository.getSupply()
+
+    fun getVersion(): Single<String> = mRealmRepository.get()
 }
