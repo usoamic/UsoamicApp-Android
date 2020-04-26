88fcf4b3660c35f87f41165352c359f676355dd4
primys <a2uso@tuta.io>
2020-04-26 10:42:17 -0700

    Add Base FrameLayout to that move some from Information View

--- app/src/main/java/io/usoamic/wallet/custom/InformationView.kt
+++ app/src/main/java/io/usoamic/wallet/custom/InformationView.kt
@@ -2,18 +2,15 @@ package io.usoamic.wallet.custom
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
-import android.widget.FrameLayout
 import io.usoamic.wallet.R
+import io.usoamic.wallet.custom.base.BaseFrameLayout
 import io.usoamic.wallet.databinding.ViewInformationBinding
 
 
 class InformationView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) :
-    FrameLayout(context, attrs, defStyleAttr) {
-    private val view: View by lazy { View.inflate(context, R.layout.view_information, this) }
-    private val binding: ViewInformationBinding by lazy { ViewInformationBinding.bind(view) }
+) : BaseFrameLayout(context, attrs, defStyleAttr, R.layout.view_information) {
+    override val binding: ViewInformationBinding by lazy { ViewInformationBinding.bind(requireView()) }
 
     init {
         val a = context.obtainStyledAttributes(
