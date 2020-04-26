e2a6c50596f09d9643210f7f2a0d621cf35ee174
primys <a2uso@tuta.io>
2020-04-26 11:23:37 -0700

    Add ui for private key and address

--- app/src/main/java/io/usoamic/wallet/ui/main/create/CreateFragment.kt
+++ app/src/main/java/io/usoamic/wallet/ui/main/create/CreateFragment.kt
@@ -5,16 +5,18 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import io.usoamic.wallet.R
 import io.usoamic.wallet.UsoamicWallet
+import io.usoamic.wallet.custom.listview.model.ListItem
 import io.usoamic.wallet.databinding.FragmentCreateBinding
 import io.usoamic.wallet.di.other.ViewModelFactory
+import io.usoamic.wallet.domain.models.create.AccountData
+import io.usoamic.wallet.extensions.observe
 import io.usoamic.wallet.ui.base.BaseViewModelFragment
-import io.usoamic.wallet.ui.main.add.AddViewModel
 import javax.inject.Inject
 
 class CreateFragment : BaseViewModelFragment(R.layout.fragment_create) {
     @Inject
-    lateinit var viewModelFactory: ViewModelFactory<AddViewModel>
-    override val viewModel: AddViewModel by viewModels { viewModelFactory }
+    lateinit var viewModelFactory: ViewModelFactory<CreateViewModel>
+    override val viewModel: CreateViewModel by viewModels { viewModelFactory }
 
     lateinit var binding: FragmentCreateBinding
 
@@ -25,6 +27,17 @@ class CreateFragment : BaseViewModelFragment(R.layout.fragment_create) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar()
+    }
+
+    override fun initObservers() {
+        observe(viewModel.ldContent, ::setAccountData)
+    }
+
+    private fun setAccountData(data: AccountData) {
+        binding.listView.addAll(listOf(
+            ListItem(getString(R.string.address), data.address),
+            ListItem(getString(R.string.private_key), data.privateKey)
+        ))
     }
 
     override fun initBinding() {
--- app/src/main/java/io/usoamic/wallet/ui/main/create/CreateViewModel.kt
+++ app/src/main/java/io/usoamic/wallet/ui/main/create/CreateViewModel.kt
@@ -1,6 +1,8 @@
 package io.usoamic.wallet.ui.main.create
 
+import androidx.lifecycle.MutableLiveData
 import io.usoamic.wallet.domain.models.NavDirections
+import io.usoamic.wallet.domain.models.create.AccountData
 import io.usoamic.wallet.ui.base.BaseViewModel
 import io.usoamic.wallet.usecases.CreateAccountUseCase
 import javax.inject.Inject
@@ -9,7 +11,9 @@ class CreateViewModel @Inject constructor(
     private val mModel: CreateAccountUseCase,
     private val mArguments: NavDirections.Add
 ) : BaseViewModel() {
-    init {
+    val ldContent = MutableLiveData<AccountData>()
 
+    init {
+        ldContent.value = AccountData("0x", "0x0")
     }
 }
