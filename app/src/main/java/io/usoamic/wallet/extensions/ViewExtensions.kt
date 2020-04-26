1f3a8d64e9d962bd4b5e37d6491d2dbbcfd53481
primys <a2uso@tuta.io>
2020-04-26 11:21:12 -0700

    Add extensions for manage visibility

--- app/src/main/java/io/usoamic/wallet/extensions/ViewExtensions.kt
+++ app/src/main/java/io/usoamic/wallet/extensions/ViewExtensions.kt
@@ -1,6 +1,10 @@
 package io.usoamic.wallet.extensions
 
+import android.view.View
 import androidx.annotation.StringRes
+import androidx.core.view.isGone
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 
@@ -9,6 +13,16 @@ fun Fragment.showMessage(message: String) {
     Snackbar.make(requireView(), message, Snackbar.LENGTH_LONG).show()
 }
 
-fun Fragment.showMessage(@StringRes resId: Int) = showMessage(string(resId))
+fun Fragment.showMessage(@StringRes resId: Int) = showMessage(getString(resId))
 
-fun Fragment.string(@StringRes resId: Int) = getString(resId)
+fun View.show() {
+    isVisible = true
+}
+
+fun View.gone() {
+    isGone = true
+}
+
+fun View.invisible() {
+    isInvisible = true
+}
