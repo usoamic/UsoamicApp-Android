f9bbdcf4bb1d40e901a4eeac9ea7c43835af59ff
primys <a2uso@tuta.io>
2020-05-01 15:47:40 -0700

    Add methods hasAccount() and getAddress() to UserRepository

--- app/src/main/java/io/usoamic/wallet/domain/repositories/UserRepository.kt
+++ app/src/main/java/io/usoamic/wallet/domain/repositories/UserRepository.kt
@@ -1,4 +1,8 @@
 package io.usoamic.wallet.domain.repositories
 
+import io.reactivex.rxjava3.core.Single
+
 interface UserRepository {
+    fun hasAccount(): Single<Boolean>
+    fun getAddress(): Single<String>
 }
--- app/src/main/java/io/usoamic/wallet/domain/repositories/UserRepositoryImpl.kt
+++ app/src/main/java/io/usoamic/wallet/domain/repositories/UserRepositoryImpl.kt
@@ -1,8 +1,19 @@
 package io.usoamic.wallet.domain.repositories
 
+import io.reactivex.rxjava3.core.Single
 import io.usoamic.usoamickt.core.Usoamic
 import javax.inject.Inject
 
-class UserRepositoryImpl @Inject constructor(usoamic: Usoamic) : UserRepository {
+class UserRepositoryImpl @Inject constructor(private val usoamic: Usoamic) : UserRepository {
+    override fun hasAccount(): Single<Boolean> {
+        return Single.fromCallable {
+            usoamic.hasAccount
+        }
+    }
 
+    override fun getAddress(): Single<String> {
+        return Single.fromCallable {
+            usoamic.address
+        }
+    }
 }
