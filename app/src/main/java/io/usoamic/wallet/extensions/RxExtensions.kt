92ba294188e62e72c4c7234bf4db0f304173b350
primys <a2uso@tuta.io>
2020-05-01 14:47:29 -0700

    Fix subscribeOnIo

--- app/src/main/java/io/usoamic/wallet/extensions/RxExtensions.kt
+++ app/src/main/java/io/usoamic/wallet/extensions/RxExtensions.kt
@@ -1,8 +1,10 @@
 package io.usoamic.wallet.extensions
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 fun <T> Single<T>.observeOnMain() = observeOn(AndroidSchedulers.mainThread())
 
-fun <T> Single<T>.subscribeOnIo() = observeOn(AndroidSchedulers.mainThread())
+fun <T> Single<T>.subscribeOnIo() = observeOn(Schedulers.io())
