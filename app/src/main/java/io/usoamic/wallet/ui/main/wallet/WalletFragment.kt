503bdfab8a92154470672fe2398f38ecbc0c822e
primys <a2uso@tuta.io>
2020-05-01 15:45:57 -0700

    Add WalletFragment

---
+++ app/src/main/java/io/usoamic/wallet/ui/main/wallet/WalletFragment.kt
@@ -0,0 +1,7 @@
+package io.usoamic.wallet.ui.main.wallet
+
+import io.usoamic.wallet.R
+import io.usoamic.wallet.ui.base.BaseFragment
+
+class WalletFragment : BaseFragment(R.layout.fragment_wallet) {
+}
