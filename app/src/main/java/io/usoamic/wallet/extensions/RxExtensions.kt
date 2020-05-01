5b7e3e907c71d1177dfa0c0f76f714f5b7f9f826
primys <a2uso@tuta.io>
2020-05-01 14:48:27 -0700

    Set specified type for extensions

--- app/src/main/java/io/usoamic/wallet/extensions/RxExtensions.kt
+++ app/src/main/java/io/usoamic/wallet/extensions/RxExtensions.kt
@@ -1,10 +1,9 @@
 package io.usoamic.wallet.extensions
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-fun <T> Single<T>.observeOnMain() = observeOn(AndroidSchedulers.mainThread())
+fun <T> Single<T>.observeOnMain(): Single<T> = observeOn(AndroidSchedulers.mainThread())
 
-fun <T> Single<T>.subscribeOnIo() = observeOn(Schedulers.io())
+fun <T> Single<T>.subscribeOnIo(): Single<T> = observeOn(Schedulers.io())
