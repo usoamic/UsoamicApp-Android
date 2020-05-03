40c8372db157d020dace90d147a21655f89fd5c1
primys <a2uso@tuta.io>
2020-05-03 12:44:03 -0700

    Add LiveEvent dependency and extension emit()

--- app/src/main/java/io/usoamic/wallet/extensions/LifecycleExtensions.kt
+++ app/src/main/java/io/usoamic/wallet/extensions/LifecycleExtensions.kt
@@ -3,9 +3,14 @@ package io.usoamic.wallet.extensions
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import com.hadilq.liveevent.LiveEvent
 
 fun <T> Fragment.observe(ld: MutableLiveData<T>, callback: (T) -> Unit) {
     ld.observe(viewLifecycleOwner, Observer<T> {
         callback.invoke(it)
     })
+}
+
+fun LiveEvent<Unit>.emit() {
+    value = Unit
 }
