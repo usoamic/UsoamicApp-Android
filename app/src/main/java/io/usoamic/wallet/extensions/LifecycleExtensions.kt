8d71af92bb202213e0f52d6ee2a9ecd3ef36ab55
primys <a2uso@tuta.io>
2020-05-07 12:26:00 -0700

    Add observe extension for Activity

--- app/src/main/java/io/usoamic/wallet/extensions/LifecycleExtensions.kt
+++ app/src/main/java/io/usoamic/wallet/extensions/LifecycleExtensions.kt
@@ -1,12 +1,17 @@
 package io.usoamic.wallet.extensions
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.hadilq.liveevent.LiveEvent
 
 fun <T> Fragment.observe(ld: MutableLiveData<T>, callback: (T) -> Unit) {
-    ld.observe(viewLifecycleOwner, Observer<T> {
+    viewLifecycleOwner.observe(ld, callback)
+}
+
+fun <T> LifecycleOwner.observe(ld: MutableLiveData<T>, callback: (T) -> Unit) {
+    ld.observe(this, Observer<T> {
         callback.invoke(it)
     })
 }
