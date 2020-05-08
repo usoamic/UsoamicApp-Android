b554ceff1e8381376bbad452d81fa1c6fefdc81d
primys <a2uso@tuta.io>
2020-05-08 12:00:28 -0700

    Add methods removePreferences() and removeAccount()

--- app/src/main/java/io/usoamic/wallet/usecases/UnlockUseCase.kt
+++ app/src/main/java/io/usoamic/wallet/usecases/UnlockUseCase.kt
@@ -3,6 +3,7 @@ package io.usoamic.wallet.usecases
 import io.reactivex.rxjava3.core.Single
 import io.usoamic.wallet.domain.repositories.EthereumRepository
 import io.usoamic.wallet.domain.repositories.PreferencesRepository
+import io.usoamic.wallet.domain.repositories.UserRepository
 import io.usoamic.wallet.domain.repositories.ValidateRepository
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneOffset
@@ -11,6 +12,7 @@ import javax.inject.Inject
 class UnlockUseCase @Inject constructor(
     private val mValidateRepository: ValidateRepository,
     private val mEthereumRepository: EthereumRepository,
+    private val mUserRepository: UserRepository,
     private val mPreferencesRepository: PreferencesRepository
 ) {
     fun getAddress(password: String): Single<String> {
@@ -27,4 +29,8 @@ class UnlockUseCase @Inject constructor(
         mPreferencesRepository.setAddress(address)
         mPreferencesRepository.setUnlockTime(LocalDateTime.now(ZoneOffset.UTC))
     }
+
+    fun removePreferences() = mPreferencesRepository.removeAll()
+
+    fun removeAccount() = mUserRepository.removeAccount()
 }
