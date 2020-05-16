29a7e52c84d9f56acf93fe557e39e91f84b77674
primys <a2uso@tuta.io>
2020-05-16 12:59:03 -0700

    Add mock content for Dashboard

--- app/src/main/java/io/usoamic/wallet/ui/main/dashboard/DashboardViewModel.kt
+++ app/src/main/java/io/usoamic/wallet/ui/main/dashboard/DashboardViewModel.kt
@@ -1,10 +1,33 @@
 package io.usoamic.wallet.ui.main.dashboard
 
+import androidx.lifecycle.MutableLiveData
 import io.usoamic.wallet.ui.base.BaseViewModel
+import io.usoamic.wallet.ui.main.dashboard.adapter.DashboardItem
+import io.usoamic.wallet.ui.main.dashboard.adapter.DashboardItemType
 import javax.inject.Inject
 
 class DashboardViewModel @Inject constructor() : BaseViewModel() {
-    init {
+    val ldData = MutableLiveData<List<DashboardItem>>()
 
+    init {
+        ldData.value = listOf(
+            DashboardItem(
+                DashboardItemType.UsoBalance,
+                "111"
+            ),
+            DashboardItem(
+                DashboardItemType.EthBalance,
+                "222"
+            ),
+            DashboardItem(
+                DashboardItemType.Height,
+                "333"
+            ),
+            DashboardItem(
+                DashboardItemType.Supply,
+                "444"
+            )
+
+        )
     }
 }
