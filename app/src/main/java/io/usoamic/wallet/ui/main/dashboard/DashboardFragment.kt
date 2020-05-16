ec4e8d7319a0a6610614cc9c3d8c4279b6934701
primys <a2uso@tuta.io>
2020-05-16 13:16:57 -0700

    Implement Dashboard fragment

--- app/src/main/java/io/usoamic/wallet/ui/main/dashboard/DashboardFragment.kt
+++ app/src/main/java/io/usoamic/wallet/ui/main/dashboard/DashboardFragment.kt
@@ -1,12 +1,18 @@
 package io.usoamic.wallet.ui.main.dashboard
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import io.usoamic.wallet.R
 import io.usoamic.wallet.UsoamicWallet
 import io.usoamic.wallet.databinding.FragmentDashboardBinding
 import io.usoamic.wallet.di.other.ViewModelFactory
+import io.usoamic.wallet.extensions.observe
 import io.usoamic.wallet.ui.base.BaseViewModelFragment
+import io.usoamic.wallet.ui.main.dashboard.adapter.DashboardAdapter
+import io.usoamic.wallet.ui.main.dashboard.adapter.DashboardItem
 import javax.inject.Inject
 
 class DashboardFragment : BaseViewModelFragment(R.layout.fragment_dashboard) {
@@ -17,7 +23,41 @@ class DashboardFragment : BaseViewModelFragment(R.layout.fragment_dashboard) {
         FragmentDashboardBinding.bind(it.requireView())
     }
 
+    private val dashboardAdapter = DashboardAdapter()
+
     override fun inject() {
         UsoamicWallet.component.dashboardSubcomponent.create().inject(this)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
+
+    override fun initObservers() {
+        super.initObservers()
+        observe(viewModel.ldData, ::setData)
+        observe(viewModel.ldShowSwipeRefresh, ::showSwipeRefresh)
+    }
+
+    override fun initListeners() {
+        binding.srLayout.setOnRefreshListener {
+            viewModel.onRefresh()
+        }
+    }
+
+    private fun setData(list: List<DashboardItem>) {
+        dashboardAdapter.addAll(list)
+    }
+
+    private fun showSwipeRefresh(isProgress: Boolean) {
+        binding.srLayout.isRefreshing = isProgress
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = dashboardAdapter
+        }
     }
 }
