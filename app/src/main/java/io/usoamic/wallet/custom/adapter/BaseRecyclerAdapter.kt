88116f4bd5f4fd223187f5325188a9295d786f3d
primys <a2uso@tuta.io>
2020-05-16 12:56:19 -0700

    Move BaseRecyclerAdapter and BaseViewHolder to custom/adapter

--- app/src/main/java/io/usoamic/wallet/custom/adapter/BaseRecyclerAdapter.kt
+++ app/src/main/java/io/usoamic/wallet/custom/adapter/BaseRecyclerAdapter.kt
@@ -1,14 +1,18 @@
-package io.usoamic.wallet.ui.base
+package io.usoamic.wallet.custom.adapter
 
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class BaseRecyclerAdapter<VH : RecyclerView.ViewHolder, T> : RecyclerView.Adapter<VH>() {
+abstract class BaseRecyclerAdapter<T, VH : BaseViewHolder<T>> : RecyclerView.Adapter<VH>() {
     protected val items = mutableListOf<T>()
 
     fun addAll(data: List<T>) {
         items.clear()
         items.addAll(data)
         notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.bind(items[position])
     }
 
     override fun getItemCount(): Int = items.size
--- app/src/main/java/io/usoamic/wallet/custom/adapter/BaseViewHolder.kt
+++ app/src/main/java/io/usoamic/wallet/custom/adapter/BaseViewHolder.kt
@@ -1,8 +1,11 @@
-package io.usoamic.wallet.ui.base
+package io.usoamic.wallet.custom.adapter
 
+import android.content.Context
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class BaseViewHolder<T>(open val view: View) : RecyclerView.ViewHolder(view) {
-    protected abstract fun bind(item: T)
+    val context: Context get() = view.context
+
+    abstract fun bind(item: T)
 }
