1e232e8e24537e9737ff1a6926e55f745643f9d8
primys <a2uso@tuta.io>
2020-06-21 07:54:44 -0700

    Inject DashboardUseCase

--- app/src/main/java/io/usoamic/wallet/ui/main/dashboard/DashboardViewModel.kt
+++ app/src/main/java/io/usoamic/wallet/ui/main/dashboard/DashboardViewModel.kt
@@ -4,9 +4,12 @@ import androidx.lifecycle.MutableLiveData
 import io.usoamic.wallet.ui.base.BaseViewModel
 import io.usoamic.wallet.ui.main.dashboard.adapter.DashboardItem
 import io.usoamic.wallet.ui.main.dashboard.adapter.DashboardItemType
+import io.usoamic.wallet.usecases.DashboardUseCase
 import javax.inject.Inject
 
-class DashboardViewModel @Inject constructor() : BaseViewModel() {
+class DashboardViewModel @Inject constructor(
+    private val mDashboardUseCase: DashboardUseCase
+) : BaseViewModel() {
     val ldData = MutableLiveData<List<DashboardItem>>()
     val ldShowSwipeRefresh = MutableLiveData<Boolean>()
 
