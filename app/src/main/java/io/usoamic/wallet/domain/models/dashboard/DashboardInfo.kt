8e7a09efd3d3cdbd9697210ea53a8bb2c0955166
primys <a2uso@tuta.io>
2020-06-21 12:01:45 -0700

    Add extensions for conversation and DEFAULT object

--- app/src/main/java/io/usoamic/wallet/domain/models/dashboard/DashboardInfo.kt
+++ app/src/main/java/io/usoamic/wallet/domain/models/dashboard/DashboardInfo.kt
@@ -1,5 +1,6 @@
 package io.usoamic.wallet.domain.models.dashboard
 
+import io.usoamic.wallet.domain.models.realm.DashboardInfoRealm
 import java.math.BigDecimal
 import java.math.BigInteger
 
@@ -8,4 +9,27 @@ data class DashboardInfo(
     val usoBalance: BigDecimal,
     val height: BigInteger,
     val supply: BigDecimal
+) {
+    companion object {
+        val DEFAULT = DashboardInfo(
+            BigDecimal(111),
+            BigDecimal(222),
+            BigInteger.valueOf(333),
+            BigDecimal(444)
+        )
+    }
+}
+
+fun DashboardInfoRealm.toDomain() = DashboardInfo(
+    ethBalance = BigDecimal(ethBalance),
+    usoBalance = BigDecimal(usoBalance),
+    height = BigInteger(height),
+    supply = BigDecimal(supply)
+)
+
+fun DashboardInfo.toRealm() = DashboardInfoRealm(
+    ethBalance = ethBalance.toPlainString(),
+    usoBalance = usoBalance.toPlainString(),
+    height = height.toString(),
+    supply = supply.toPlainString()
 )
