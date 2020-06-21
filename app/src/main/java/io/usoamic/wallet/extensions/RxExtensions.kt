a3d9ecfc02a4e14fec1f6ca1251c5ca3e6513434
primys <a2uso@tuta.io>
2020-06-21 11:29:22 -0700

    Fix addDebugDelay

--- app/src/main/java/io/usoamic/wallet/extensions/RxExtensions.kt
+++ app/src/main/java/io/usoamic/wallet/extensions/RxExtensions.kt
@@ -3,6 +3,7 @@ package io.usoamic.wallet.extensions
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import io.usoamic.wallet.BuildConfig
 import java.util.concurrent.TimeUnit
 
 fun <T> Single<T>.observeOnMain(): Single<T> = observeOn(AndroidSchedulers.mainThread())
@@ -11,4 +12,9 @@ fun <T> Single<T>.subscribeOnIo(): Single<T> = subscribeOn(Schedulers.io())
 
 fun <T> Single<T>.addSchedulers(): Single<T> = subscribeOnIo().observeOnMain()
 
-fun <T> Single<T>.addDebugDelay(): Single<T> = delay(2, TimeUnit.SECONDS)
+fun <T> Single<T>.addDebugDelay(): Single<T> {
+    if(BuildConfig.DEBUG) {
+        return delay(2, TimeUnit.SECONDS)
+    }
+    return this
+}
