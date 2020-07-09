885f65f68138f9668e2f363005d9bccf7ecfb269
primys <a2uso@tuta.io>
2020-07-09 14:16:01 -0700

    Add SwipeRefreshLayout, ProgressBar and other

--- app/src/main/java/io/usoamic/wallet/ui/main/history/HistoryFragment.kt
+++ app/src/main/java/io/usoamic/wallet/ui/main/history/HistoryFragment.kt
@@ -1,23 +1,36 @@
 package io.usoamic.wallet.ui.main.history
 
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import by.kirich1409.viewbindingdelegate.viewBinding
 import io.usoamic.wallet.R
 import io.usoamic.wallet.UsoamicWallet
 import io.usoamic.wallet.databinding.FragmentHistoryBinding
 import io.usoamic.wallet.di.other.ViewModelFactory
-import io.usoamic.wallet.ui.base.BaseViewModelFragment
+import io.usoamic.wallet.ui.base.BaseSrViewModelFragment
 import javax.inject.Inject
 
-class HistoryFragment : BaseViewModelFragment(R.layout.fragment_history) {
+class HistoryFragment : BaseSrViewModelFragment(R.layout.fragment_history) {
     @Inject
     lateinit var viewModelFactory: ViewModelFactory<HistoryViewModel>
     override val viewModel: HistoryViewModel by viewModels { viewModelFactory }
+    override val srLayout: SwipeRefreshLayout
+        get() = binding.srLayout
+
     override val binding: FragmentHistoryBinding by viewBinding {
         FragmentHistoryBinding.bind(it.requireView())
     }
 
     override fun inject() {
         UsoamicWallet.component.historySubcomponent.create().inject(this)
+    }
+
+    override fun showProgress(isProgress: Boolean) {
+        binding.apply {
+            pbContainer.progressBar.isVisible = isProgress
+            srLayout.isInvisible = isProgress
+        }
     }
 }
--- app/src/main/java/io/usoamic/wallet/ui/main/history/HistoryViewModel.kt
+++ app/src/main/java/io/usoamic/wallet/ui/main/history/HistoryViewModel.kt
@@ -1,10 +1,14 @@
 package io.usoamic.wallet.ui.main.history
 
-import io.usoamic.wallet.ui.base.BaseViewModel
+import io.usoamic.wallet.ui.base.BaseSrViewModel
+import io.usoamic.wallet.usecases.HistoryUseCase
 import javax.inject.Inject
 
-class HistoryViewModel @Inject constructor() : BaseViewModel() {
+class HistoryViewModel @Inject constructor(
+    private val mUseCase: HistoryUseCase
+) : BaseSrViewModel() {
     init {
 
     }
+
 }
