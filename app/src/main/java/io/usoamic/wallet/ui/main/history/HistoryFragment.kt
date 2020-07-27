3234b053db3f4687bd2571779ae57106d8910c49
primys <a2uso@tuta.io>
2020-07-27 12:27:47 -0700

    Implement HistoryFragment

--- app/src/main/java/io/usoamic/wallet/ui/main/history/HistoryFragment.kt
+++ app/src/main/java/io/usoamic/wallet/ui/main/history/HistoryFragment.kt
@@ -1,15 +1,21 @@
 package io.usoamic.wallet.ui.main.history
 
+import android.os.Bundle
+import android.view.View
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import by.kirich1409.viewbindingdelegate.viewBinding
 import io.usoamic.wallet.R
 import io.usoamic.wallet.UsoamicWallet
 import io.usoamic.wallet.databinding.FragmentHistoryBinding
 import io.usoamic.wallet.di.other.ViewModelFactory
+import io.usoamic.wallet.domain.models.history.TransactionItem
+import io.usoamic.wallet.extensions.observe
 import io.usoamic.wallet.ui.base.BaseSrViewModelFragment
+import io.usoamic.wallet.ui.main.history.adapter.HistoryAdapter
 import javax.inject.Inject
 
 class HistoryFragment : BaseSrViewModelFragment(R.layout.fragment_history) {
@@ -19,12 +25,35 @@ class HistoryFragment : BaseSrViewModelFragment(R.layout.fragment_history) {
     override val srLayout: SwipeRefreshLayout
         get() = binding.srLayout
 
+    private val historyAdapter = HistoryAdapter()
+
     override val binding: FragmentHistoryBinding by viewBinding {
         FragmentHistoryBinding.bind(it.requireView())
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
+
     override fun inject() {
         UsoamicWallet.component.historySubcomponent.create().inject(this)
+    }
+
+    override fun initObservers() {
+        super.initObservers()
+        observe(viewModel.ldData, ::setData)
+    }
+
+    private fun setData(list: List<TransactionItem>) {
+        historyAdapter.addAll(list)
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = historyAdapter
+        }
     }
 
     override fun showProgress(isProgress: Boolean) {
--- app/src/main/java/io/usoamic/wallet/ui/main/history/HistoryViewModel.kt
+++ app/src/main/java/io/usoamic/wallet/ui/main/history/HistoryViewModel.kt
@@ -1,7 +1,7 @@
 package io.usoamic.wallet.ui.main.history
 
 import androidx.lifecycle.MutableLiveData
-import io.usoamic.usoamickt.model.Transaction
+import io.usoamic.wallet.domain.models.history.TransactionItem
 import io.usoamic.wallet.extensions.addSchedulers
 import io.usoamic.wallet.ui.base.BaseSrViewModel
 import io.usoamic.wallet.usecases.HistoryUseCase
@@ -10,21 +10,23 @@ import javax.inject.Inject
 class HistoryViewModel @Inject constructor(
     private val mUseCase: HistoryUseCase
 ) : BaseSrViewModel() {
-    val ldHistory = MutableLiveData<List<Transaction>>()
+    val ldData = MutableLiveData<List<TransactionItem>>()
 
     init {
-        getTransactions()
+        updateTransactions()
     }
 
-    private fun getTransactions() {
-        mUseCase.getTransactions()
+    private fun updateTransactions(force: Boolean = false) {
+        mUseCase.getTransactions(force)
             .addSchedulers()
             .addProgress()
             .subscribe(::setHistory, ::throwError)
             .addToDisposable()
     }
 
-    private fun setHistory(list: List<Transaction>) {
-        ldHistory.value = list
+    private fun setHistory(list: List<TransactionItem>) {
+        ldData.value = list
     }
+
+    override fun onRefresh() = updateTransactions(true)
 }
