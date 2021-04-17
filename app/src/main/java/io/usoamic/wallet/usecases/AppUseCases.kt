78a20aad7ba58563ed2a915b10d691b7afba9793
primys <primys@usoamic.io>
2021-04-17 10:42:42 -0700

    Renamed SingleUseCases to AppUseCases

--- app/src/main/java/io/usoamic/wallet/usecases/AppUseCases.kt
+++ app/src/main/java/io/usoamic/wallet/usecases/AppUseCases.kt
@@ -1,6 +1,7 @@
 package io.usoamic.wallet.usecases
 
 import io.reactivex.Single
+import io.usoamic.commons.crossplatform.api.DateCompat
 import io.usoamic.commons.crossplatform.api.PreferencesCompat
 import io.usoamic.commons.crossplatform.repositories.api.PreferencesRepository
 import io.usoamic.commons.crossplatform.repositories.api.UserRepository
@@ -10,16 +11,23 @@ import org.threeten.bp.ZoneOffset
 import org.threeten.bp.temporal.ChronoUnit
 import javax.inject.Inject
 
-class SingleUseCases @Inject constructor(
+class AppUseCases @Inject constructor(
     private val mUserRepository: UserRepository,
-    private val mPreferencesRepository: PreferencesRepository
+    private val mPreferencesRepository: PreferencesRepository,
+    private val mDateCompat: DateCompat
 ) {
     fun hasAccount(): Single<Boolean> {
         return mUserRepository.hasAccount()
     }
 
+    fun updateLastAction() {
+        if(!isNeedLocked()) {
+            mPreferencesRepository.setLastActionTime(mDateCompat.currentTimestamp)
+        }
+    }
+
     fun isNeedLocked(): Boolean {
-        val timestamp = mPreferencesRepository.getUnlockTime()
+        val timestamp = getLastActionTime()
         val unlockTime = LocalDateTime.ofEpochSecond(timestamp, 0, ZoneOffset.UTC)
         val currentTime = LocalDateTime.now(ZoneOffset.UTC)
         val between = ChronoUnit.SECONDS.between(unlockTime, currentTime)
@@ -30,7 +38,9 @@ class SingleUseCases @Inject constructor(
     fun lockApp() {
         with(mPreferencesRepository) {
             remove(PreferencesCompat.Key.ADDRESS)
-            remove(PreferencesCompat.Key.TIMESTAMP)
+            remove(PreferencesCompat.Key.LAST_ACTION_TIMESTAMP)
         }
     }
+
+    fun getLastActionTime(): Long = mPreferencesRepository.getLastActionTime()
 }
